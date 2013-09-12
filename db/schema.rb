e2bd4136d56060c7b3f8afb093ccabfea0454238
Chris Sutton <chris@databoxdigital.com>
2013-09-12 15:21:34 -0400

    changing to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130912161943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: true do |t|
     t.string   "title"
     t.text     "description"
